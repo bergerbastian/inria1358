5bacefae40d9f6fdbb1561fbfadc8504f2f4674d
Paul Renger <paulrenger99@gmail.com>
2023-09-21 13:18:44 +0200

    Added comments and doc string

--- ML/data.py
+++ ML/data.py
@@ -9,43 +9,65 @@ import tensorflow as tf
 from colorama import Fore, Style
 
 def make_patches(source_path, save_path, image_size=(200,200), image_type="png"):
-    isExist = os.path.exists(save_path)
+    """Function to create patches from the original images, in order to be able to feed them to the model
+        Args:
+            source_path: path to the original dataset
+            save_path: path where the patches should be saved
+            image_size: (width, height) of patches
+            image_type: file type that the patches should be outputted in
+    """
 
-    sets = []
-
-    # Check if save path exists
-    if isExist:
-
-        print(Fore.BLUE + "\nChecking local data..." + Style.RESET_ALL)
-        # Get # of images from save path
-        train_images = len(os.listdir(f"{save_path}/train/images"))
-        train_gt = len(os.listdir(f"{save_path}/train/gt"))
-        test_images = len(os.listdir(f"{save_path}/test/images"))
-
-        print(f"{train_images} Patches found for X_train")
-        print(f"{train_gt} Patches found for y_train")
-        print(f"{test_images} Patches found for X_test")
-
-
-        print("✅ Patches already exist")
-        return
-    else:
-        # No folder exist yet, so we need to make sets for both train & test
-
-        sets = ["train", "test"]
-
-    for set in sets:
-        subfolders = ['images']
-        if set == 'train':
-            subfolders.append('gt')
-
+    # Checking if directories exist, if not, create them
+    for folder in ['train','test']:
+        subfolders = ['gt', 'images'] if folder == "train" else ['images']
         for subfolder in subfolders:
-            load_path = f'{source_path}/{set}/{subfolder}'
-            save_path_n = f'{save_path}/{set}/{subfolder}'
+            load_path = f'{source_path}/{folder}/{subfolder}'
+            save_path_n = f'{save_path}/{folder}/{subfolder}'
             # Check whether the specified path exists or not
             isExist = os.path.exists(save_path_n)
             if not isExist:
                 os.makedirs(save_path_n)
+
+    # Checking the local data
+    sets = []
+    print(Fore.BLUE + "\nChecking local data..." + Style.RESET_ALL)
+
+    # Get # of images from save path
+    train_images = len(os.listdir(f"{save_path}/train/images"))
+    train_gt = len(os.listdir(f"{save_path}/train/gt"))
+    test_images = len(os.listdir(f"{save_path}/test/images"))
+
+    print(f"{train_images} Patches found for X_train")
+    print(f"{train_gt} Patches found for y_train")
+    print(f"{test_images} Patches found for X_test")
+
+    # Check if >0 images are there for each set (meaning we have data) otherwise add it to the sets that we want to patch
+    if train_images == 0 or train_gt == 0:
+        print("❗ Train folders contain 0 images")
+        sets.append("train")
+    else:
+        print("✅ Train patches already exist")
+
+    if test_images == 0:
+        print("❗ Test folders contain 0 images")
+        sets.append("train")
+    else:
+        print("✅ Test patches already exist")
+
+
+    # Iterate through the sets
+    for set in sets:
+
+        # Subfolders to iterate through, if the set is train, iterate also iterate through GT folder
+        subfolders = ['images']
+        if set == 'train':
+            subfolders.append('gt')
+
+        # Iterate through each subfolder
+        for subfolder in subfolders:
+            load_path = f'{source_path}/{set}/{subfolder}'
+            save_path_n = f'{save_path}/{set}/{subfolder}'
+
             print("-"*10)
             print(f" Iterating through folder: {subfolder}")
             print("-"*10)
@@ -63,3 +85,13 @@ def make_patches(source_path, save_path, image_size=(200,200), image_type="png")
                         else:
                             im = Image.fromarray(col[0])
                         im.save(f'{save_path_n}/{filename}__{i}__{j}.{image_type}')
+
+    # Final Output
+    # Get # of images from save path
+    train_images = len(os.listdir(f"{save_path}/train/images"))
+    train_gt = len(os.listdir(f"{save_path}/train/gt"))
+    test_images = len(os.listdir(f"{save_path}/test/images"))
+
+    print(f"{train_images} Patches found for X_train")
+    print(f"{train_gt} Patches found for y_train")
+    print(f"{test_images} Patches found for X_test")
