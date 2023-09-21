c05c9ba7c164a28a6ec42f0e7e302e8d7ae2d4cb
Paul Renger <paulrenger99@gmail.com>
2023-09-21 15:06:47 +0200

    Finished make_patches function

--- ML/data.py
+++ ML/data.py
@@ -4,17 +4,19 @@ import numpy as np
 from patchify import patchify
 import matplotlib.pyplot as plt
 import os
+import random
 from tqdm import tqdm
 import tensorflow as tf
 from colorama import Fore, Style
 
-def make_patches(source_path, save_path, image_size=(200,200), image_type="png"):
+def make_patches(source_path: str, save_path: str, image_size=(200,200), image_type="png", max_files = None):
     """Function to create patches from the original images, in order to be able to feed them to the model
         Args:
             source_path: path to the original dataset
             save_path: path where the patches should be saved
             image_size: (width, height) of patches
             image_type: file type that the patches should be outputted in
+            max_files: maximum number of images to process per subfolder (selected randomly), None = no limit
     """
 
     # Checking if directories exist, if not, create them
@@ -46,13 +48,13 @@ def make_patches(source_path, save_path, image_size=(200,200), image_type="png")
         print("❗ Train folders contain 0 images")
         sets.append("train")
     else:
-        print("✅ Train patches already exist")
+        print("ℹ️ Train patches already exist")
 
     if test_images == 0:
         print("❗ Test folders contain 0 images")
-        sets.append("train")
+        sets.append("test")
     else:
-        print("✅ Test patches already exist")
+        print("ℹ️ Test patches already exist")
 
 
     # Iterate through the sets
@@ -64,17 +66,22 @@ def make_patches(source_path, save_path, image_size=(200,200), image_type="png")
             subfolders.append('gt')
 
         # Iterate through each subfolder
+
         for subfolder in subfolders:
             load_path = f'{source_path}/{set}/{subfolder}'
             save_path_n = f'{save_path}/{set}/{subfolder}'
 
-            print("-"*10)
-            print(f" Iterating through folder: {subfolder}")
-            print("-"*10)
+            print(Fore.BLUE + f"\nIterating through folder: {set}/{subfolder}" + Style.RESET_ALL)
 
             dimensions = image_size if subfolder == "gt" else (image_size[0],image_size[1],3)
+            file_count = 0
 
-            for filename in tqdm(os.listdir(load_path)):
+            files = os.listdir(load_path)
+
+            if max_files is not None:
+                files = random.sample(files, max_files)
+
+            for filename in tqdm(files):
                 im = Image.open(f'{load_path}/{filename}')
                 imarray = np.array(im)
                 patches = patchify(imarray, dimensions, step=200)
@@ -92,6 +99,9 @@ def make_patches(source_path, save_path, image_size=(200,200), image_type="png")
     train_gt = len(os.listdir(f"{save_path}/train/gt"))
     test_images = len(os.listdir(f"{save_path}/test/images"))
 
-    print(f"{train_images} Patches found for X_train")
-    print(f"{train_gt} Patches found for y_train")
-    print(f"{test_images} Patches found for X_test")
+    if len(sets) > 0:
+        print(Fore.BLUE + f"\nCounting local images..." + Style.RESET_ALL)
+        print(f"{train_images} Patches found for X_train")
+        print(f"{train_gt} Patches found for y_train")
+        print(f"{test_images} Patches found for X_test")
+    print("✅ Patches loaded")
