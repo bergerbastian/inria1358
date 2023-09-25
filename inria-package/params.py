a8acb5ae2f1797c5e874ba3c8a1e0c94885258fe
Paul Renger <paulrenger99@gmail.com>
2023-09-25 14:30:57 +0200

    Repackaging

---
+++ inria-package/params.py
@@ -0,0 +1,9 @@
+import os
+import numpy as np
+
+##################  VARIABLES  ##################
+MODEL_TARGET = os.environ.get("MODEL_TARGET")
+
+##################  CONSTANTS  #####################
+LOCAL_DATA_PATH = "/Users/paulrenger/code/Paukhard/inria1358/raw_data/aerial_images_inria1358" #os.path.join("..", "raw_data", "aerial_images_inria1358")
+LOCAL_REGISTRY_PATH = "/Users/paulrenger/code/Paukhard/inria1358/mlops/training_outputs" #os.path.join("..", "mlops", "training_outputs")
